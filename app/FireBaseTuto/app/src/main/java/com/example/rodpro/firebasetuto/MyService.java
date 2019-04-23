1d1abc588395c679db2afe83ce35a763837c25bd
Rodrigo P <rodrigo.pulido@cosmicgo.co>
2019-04-22 23:37:42 -0500

    Add token to file preferences
    -use only one service
    -implement other method to get the notification

--- app/FireBaseTuto/app/src/main/java/com/example/rodpro/firebasetuto/MyService.java
+++ app/FireBaseTuto/app/src/main/java/com/example/rodpro/firebasetuto/MyService.java
@@ -1,12 +1,11 @@
 package com.example.rodpro.firebasetuto;
 
-
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -32,8 +31,6 @@ public class MyService extends FirebaseMessagingService {
             showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
         else showNotification(remoteMessage.getData());
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
@@ -42,13 +39,30 @@ public class MyService extends FirebaseMessagingService {
 
         }
 
-        // Check if message contains a notification payload.
+
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            String titulo = remoteMessage.getNotification().getTitle();
+            String texto = remoteMessage.getNotification().getBody();
+            showNotificationNew(titulo, texto);
         }
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+
+    }
+
+    private void showNotificationNew(String title, String text) {
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.stat_sys_warning)
+                        .setContentTitle(title)
+                        .setAutoCancel(true)
+                        .setContentText(text);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
     }
 
     private void showNotification(Map<String, String> data) {
@@ -132,5 +146,22 @@ public class MyService extends FirebaseMessagingService {
         }
 
         notificationManager.notify(0, notificationBuilder.build());
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        SharedPreferences preferences= getSharedPreferences("FILE_TOKEN", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("token_value", token);
+        editor.apply();
+
+        String token_value = preferences.getString("token_value", "No hay token!!");
+        Log.d(TAG, "Value token: " + token_value);
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
     }
 }
