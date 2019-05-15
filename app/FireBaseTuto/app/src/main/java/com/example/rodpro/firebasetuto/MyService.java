b5d269fc09bae9fb60a333c5aa330cc8e4533240
Rodrigo P <rodrigo.pulido@cosmicgo.co>
2019-05-15 18:53:28 -0500

    Fire Base Notifications 001

--- app/FireBaseTuto/app/src/main/java/com/example/rodpro/firebasetuto/MyService.java
+++ app/FireBaseTuto/app/src/main/java/com/example/rodpro/firebasetuto/MyService.java
@@ -12,40 +12,33 @@ import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.Map;
 import static android.graphics.Color.rgb;
+
 
 public class MyService extends FirebaseMessagingService {
 
-    String TAG = "Mensajes_Firebase: ";
+    String TAG = "MENSSAGE_FIRBASE";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
-        if (remoteMessage.getData().isEmpty())
-            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
-        else {
-            showNotification(remoteMessage.getData());
-        }
+
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-        }
-
         if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Title: " + remoteMessage.getNotification().getTitle());
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            showNotification(remoteMessage);
             /*String titulo = remoteMessage.getNotification().getTitle();
             String texto = remoteMessage.getNotification().getBody();
             showNotificationNew(titulo, texto);*/
         }
 
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
     }
 
     private void showNotificationNew(String title, String text) {
@@ -63,57 +56,15 @@ public class MyService extends FirebaseMessagingService {
         notificationManager.notify(0, notificationBuilder.build());
     }
 
-    private void showNotification(Map<String, String> data) {
-        String title = data.get("title").toString();
-        String body = data.get("body").toString();
-        String NOTIFCATION_CHANEL_ID = getString(R.string.default_notification_channel_id);
-
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, NOTIFCATION_CHANEL_ID);
-        notificationBuilder
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setColor(rgb(255 ,160, 0))
-                .setContentTitle(title)
-                .setContentText(body)
-                .setAutoCancel(true)
-                .setVibrate(new long[]{0, 1000, 500, 1000})
-                .setSound(soundUri)
-                .setContentIntent(pendingIntent)
-                .setContentInfo("info");
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFCATION_CHANEL_ID,
-                    "Notification",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-
-            notificationChannel.setDescription("Description");
-            notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.BLUE);
-            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-
-        notificationManager.notify(0, notificationBuilder.build());
-    }
-
-    private void showNotification(String title, String body) {
+    private void showNotification(RemoteMessage remoteMessage) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
         String NOTIFCATION_CHANEL_ID = getString(R.string.default_notification_channel_id);
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
 
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, NOTIFCATION_CHANEL_ID);
@@ -154,9 +105,6 @@ public class MyService extends FirebaseMessagingService {
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("token_value", token);
         editor.apply();
-
-        String token_value = preferences.getString("token_value", "No hay token!!");
-        Log.d(TAG, "Value token: " + token_value);
         sendRegistrationToServer(token);
     }
 
@@ -166,6 +114,9 @@ public class MyService extends FirebaseMessagingService {
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate");
+        //SharedPreferences preferences= getSharedPreferences("FILE_TOKEN", Context.MODE_PRIVATE);
+        //String token_value = preferences.getString("token_value", "No hay token!!");
+        //Log.d(TAG, "Value token: " + token_value);
         super.onCreate();
     }
 
